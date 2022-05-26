de6dd4d5cbe43f4e222ab5f865c7720a82dc9e78
YuiShoji <y_shoji@argo-aichi.com>
2022-05-26 10:16:44 +0900

    '6'

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/', function () {
 Route::get('/folders/create', 'App\Http\Controllers\FolderController@showCreateForm')->name('folders.create');
 
 Route::post('/folders/create', 'App\Http\Controllers\FolderController@create');
+
+Route::get('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@showCreateForm')->name('tasks.create');
+
+Route::post('/folders/{id}/tasks/create', 'App\Http\Controllers\TaskController@create');
